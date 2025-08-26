ece5bdbee472f36cf226b12a59b732eecae90e6d
raanyiel <cgunnar05@gmail.com>
2025-08-26 12:02:54 -0500

    fix(commands): Refactor "purge" to fix a bug
    Changed "purge" to no longer rely on unwraps with default values. Rust is the coolest programming language ever: the fact I can use returns to return from the function in a match statement that's assigning a variable, AND THEN use no return statement to specify what I want the variable to be assigned to?? I'm in shock at how beautiful this is.

--- src/commands/utility/purge.rs
+++ src/commands/utility/purge.rs
@@ -9,19 +9,26 @@ pub async fn purge(
     #[rename = "1-99"]
     num: Option<u8>,
 ) -> Result<(), Error> {
-    if num.is_none() {
-        ctx.say("Need to specify number of messages to delete")
-            .await?;
-        return Ok(());
-    } else if num.unwrap_or(0) > 99 {
-        ctx.say("Too many messages; Discord API only allows fetching 100 messages at a time (99 including the purge command).").await?;
-        return Ok(());
-    } else if num.unwrap_or(2) < 1 {
-        ctx.say("Um.. what?").await?;
-        return Ok(());
-    }
+    let to_delete = match num {
+        Some(x) => {
+            if x > 99 {
+                ctx.say("Too many messages; Discord API only allows fetching 100 messages at a time (99 including the purge command).").await?;
+                return Ok(());
+            } else if x < 1 {
+                ctx.say("Um.. what?").await?;
+                return Ok(());
+            } else {
+                x + 1
+            }
+        }
+        None => {
+            ctx.say("Need to specify number of messages to delete")
+                .await?;
+            return Ok(());
+        }
+    };
+
     let cid = ctx.channel_id();
-    let to_delete = num.unwrap() + 1;
     let msgs = cid
         .messages(ctx, GetMessages::new().limit(to_delete))
         .await?;
